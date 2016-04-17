92f30b816823af1ac80ccc7a400f0c4ee30f671c
CosineP <judahiii@gmail.com>
2016-04-16 23:33:46 -0400

    Multiple layers, buggy af
    On the numerous edge cases
    Realized I hadn't committed in a while

--- ld35.js
+++ ld35.js
@@ -2,11 +2,19 @@ var container = null;
 var game = null;
 var size = null;
 var aspectExtra;
+var resizeTimer;
 
-var paper;
-var paperObject;
+var papers = [];
+var paperGroup;
+var paperObjects = [];
+var options = [];
+var optionGroup;
+var optionObjects = [];
+var gridGroup;
 var foldLine = [[0,0],[0,0]];
 var foldLineObject;
+
+var mode = "line";
 
 function initialize()
 {
@@ -24,115 +32,296 @@ function initialize()
 	resize();
 	window.onresize = resizeCallback;
 
-	paper = [[16,16], [112,16], [112,112], [16,112]];
-	paperObject = game.polygon(paper).fill("#fff");
+	papers.push([[16,16], [112,16], [112,112], [16,112]]);
+	paperGroup = game.group().fill("#fff");
+	paperObjects.push(paperGroup.polygon(papers[0]));
+	optionGroup = game.group().fill("#fff");
 	foldLineObject = game.line([[0, 0], [0, 0]]).stroke({color:"#f698ec", width: 0.5});
     var gridStyle = { color: "#bbb", width: 0.2 };
+	gridGroup = game.group()
     for (var i=0; i<=128; i+=8)
     {
-        game.line(i, 0, i, 128).stroke(gridStyle);
-        game.line(0, i, 128, i).stroke(gridStyle);
+        gridGroup.line(i, 0, i, 128).stroke(gridStyle);
+        gridGroup.line(0, i, 128, i).stroke(gridStyle);
     }
 
 }
 
 function mouseDown(e)
 {
-	foldLine[0] = [Math.round(gameX(e.clientX)/8)*8, Math.round(gameY(e.clientY)/8)*8];
-	e.preventDefault();
-	hovered(e);
+	if (mode == "line")
+	{
+		foldLine[0] = [Math.round(gameX(e.clientX)/8)*8, Math.round(gameY(e.clientY)/8)*8];
+		e.preventDefault();
+		hovered(e);
+	}
 }
 function hovered(e)
 {
-	if (e.buttons)
+	if (mode == "line")
 	{
-		foldLine[1] = [gameX(e.clientX), gameY(e.clientY)];
-		foldLineObject.plot(foldLine);
+		if (e.buttons)
+		{
+			foldLine[1] = [gameX(e.clientX), gameY(e.clientY)];
+			foldLineObject.plot(foldLine);
+		}
+	}
+	else if (mode == "pick")
+	{
+		pick(e);
+	}
+	else
+	{
+		alert("Mode not specified!");
 	}
 }
 function mouseUp(e)
 {
-	foldLine[1][0] = Math.round(gameX(e.clientX)/8)*8;
-	foldLine[1][1] = Math.round(gameY(e.clientY)/8)*8;
-	var m = slope(foldLine); // Slope
-	var b = foldLine[0][1] - m*foldLine[0][0]; // Equation for y-intercept
-	var firstPoint = paper[0];
-	var intersections = [];
-	for (var i=0; i<paper.length; ++i)
+
+	if (mode == "line")
 	{
-		var nextPoint;
-		if (i != paper.length-1)
-			nextPoint = paper[i+1]
-		else
-			nextPoint = firstPoint;
-		var n = slope([paper[i], nextPoint]);
-		var x;
-		var y;
-		if (n != null)
+		foldLine[1][0] = Math.round(gameX(e.clientX)/8)*8;
+		foldLine[1][1] = Math.round(gameY(e.clientY)/8)*8;
+		split();
+		mode = "pick";
+		foldLineObject.plot(foldLine);
+	}
+	else if (mode == "pick")
+	{
+		fold(pick(e));
+		colorLayers();
+		mode = "line";
+	}
+	else
+	{
+		alert("Mode not set by default!")
+	}
+
+}
+
+function split()
+{
+
+	// Fold each layer of the paper
+	for (var layer=0; layer<papers.length; ++layer)
+	{
+		var paper = papers[layer];
+		var intersections = 0;
+		var option = [];
+		var firstPoint = paper[0];
+		var m = slope(foldLine);
+		var b = yIntercept(m, foldLine[0]);
+		var paperUp;
+		var epsilon = 0.001;
+		if (m != null)
 		{
-			var c = paper[i][1] - n*paper[i][0];
+			paperUp = firstPoint[1] + epsilon > m*firstPoint[0]+b;
+			// Lands directly on the line.
+			while (Math.abs(paper[1][1] - m*paper[1][0]+b) < epsilon)
+			{
+				if (Math.abs(paper[1][1] - m*paper[1][0]+b) < epsilon)
+					paperUp = false;
+				else
+					paperUp = true;
+			}
+		}
+		else
+		{
+			paperUp = firstPoint[0] - epsilon < foldLine[0][0];
+			// Lands directly on the line.
+			if (paperUp && firstPoint[0] + epsilon > foldLine[0][0])
+			{
+				if (Math.abs(paper[1][0] - foldLine[0][0]) < epsilon)
+					paperUp = false;
+				else
+					paperUp = true;
+			}
+		}
+		for (var i=0; i<paper.length; ++i)
+		{
+			var nextPoint;
+			if (i != paper.length-1)
+				nextPoint = paper[i+1]
+			else
+				nextPoint = firstPoint;
+			intersection = intersect([paper[i], nextPoint], foldLine);
+			var x = intersection[0];
+			var y = intersection[1];
+			// Checks if the intersect is within the bounds of both line segments
+			var intersected = (
+				x >= Math.min(nextPoint[0], paper[i][0]) && x <= Math.max(nextPoint[0], paper[i][0]) &&
+				y >= Math.min(nextPoint[1], paper[i][1]) && y <= Math.max(nextPoint[1], paper[i][1]) &&
+				x >= Math.min(foldLine[0][0], foldLine[1][0]) && x <= Math.max(foldLine[0][0], foldLine[1][0]) &&
+				y >= Math.min(foldLine[0][1], foldLine[1][1]) && y <= Math.max(foldLine[0][1], foldLine[1][1]));
+			// (intersection[0] != nextPoint[0] || intersection[1] != nextPoint[1])) &&
+			// (intersection[0] != paper[i][0] || intersection[1] != paper[i][1]
+			if ((intersections == 1 && paperUp) || (intersections != 1 && !paperUp))
+			{
+				option.push(paper[i]);
+				paper.splice(i--, 1);
+			}
+			if (intersected)
+			{
+				game.circle(3).move(x, y).fill("#0f0");
+				//if (((intersection[0] != nextPoint[0] || intersection[1] != nextPoint[1])) &&
+				//	(intersection[0] != paper[i][0] || intersection[1] != paper[i][1]))
+				{
+					paper.splice(++i, 0, intersection);
+					option.push(intersection);
+				}
+				++intersections;
+			}
+		}
+		if (paper.length <= 2)
+		{
+			paperObjects[layer].remove();
+			paperObjects.splice(layer, 1);
+			papers.splice(layer--, 1);
+		}
+		else
+		{
+			paperObjects[layer].plot(paper);
+		}
+		if (option.length > 2)
+		{
+			optionObjects.push(optionGroup.polygon(option));
+			options.push(option);
+		}
+
+	}
+	paperGroup.fill("#00f");
+
+}
+
+// Returns true if paper section is selected, false if option section
+function pick(e)
+{
+
+	// The piece containing paper[0] is by default the bottom layer
+	// Determine if this corner is above or below the line, in order to map mouse position to layer
+	var m = slope(foldLine);
+	var b = yIntercept(m, foldLine[0]);
+	var mouseUp;
+	if (m != null)
+		mouseUp = gameY(e.clientY) > m*gameX(e.clientX)+b;
+	else
+		mouseUp = gameX(e.clientX) < foldLine[0][0];
+	if (mouseUp)
+	{
+		colorLayers(true);
+		optionGroup.fill("#fff");
+		return true;
+	}
+	else
+	{
+		colorLayers(false);
+		optionGroup.fill("#f698ec");
+		return false;
+	}
+
+}
+
+function fold(paperSelected)
+{
+	var m = slope(foldLine);
+	var b = yIntercept(m, foldLine[0]); // Equation for y-intercept
+	var pieces;
+	if (paperSelected)
+		pieces = papers;
+	else
+		pieces = options;
+	for (var layer=0; layer<pieces.length; ++layer)
+	{
+		var piece = pieces[layer];
+		for (var i=0; i<piece.length; ++i)
+		{
 			if (m != null)
 			{
-				x = (c-b)/(m-n); // Equation for intersection of a line
-				y = m*x+b;
+				// Algorithm for reflecting point across line
+				var d = (piece[i][0] + (piece[i][1]-b)*m)/(1 + m*m);
+				var newX = 2*d - piece[i][0];
+				var newY = 2*d*m - piece[i][1] + 2*b;
+				piece[i] = [newX, newY];
 			}
 			else
 			{
-				x = foldLine[0][0];
-				y = n*x+c;
+				// Vertical line, just slide over
+				piece[i] = [-piece[i][0]+2*foldLine[0][0], piece[i][1]];
 			}
 		}
-		else if (m != null)
-		{
-			x = paper[i][0]; // Vertical line: x is any point on it
-			y = m*x+b;
-		}
-		// Checks if the intersect is within the bounds of both line segments
-		var intersected = (
-			x >= Math.min(nextPoint[0], paper[i][0]) && x <= Math.max(nextPoint[0], paper[i][0]) &&
-			y >= Math.min(nextPoint[1], paper[i][1]) && y <= Math.max(nextPoint[1], paper[i][1]) &&
-			x >= Math.min(foldLine[0][0], foldLine[1][0]) && x <= Math.max(foldLine[0][0], foldLine[1][0]) &&
-			y >= Math.min(foldLine[0][1], foldLine[1][1]) && y <= Math.max(foldLine[0][1], foldLine[1][1]));
-		if (intersected)
-		{
-			game.circle(3).move(x, y).fill("#0f0");
-			paper.splice(++i, 0, [x, y]);
-			intersections.push({"i":i, "x":x, "y":y});
-		}
 	}
-
-	paperObject.plot(paper);
-	foldLineObject.plot(foldLine);
+	for (var layer=0; layer<options.length; ++layer)
+	{
+		var entry = (!paperSelected)*papers.length + paperSelected*layer;
+		papers.splice(entry, 0, options[layer]);
+		paperObjects.splice(entry, 0, optionObjects[layer]);
+		paperGroup.add(paperObjects[entry]);
+	}
+	for (var layer=0; layer<paperObjects.length; ++layer)
+	{
+		paperObjects[layer].plot(papers[layer]);
+	}
+	options = [];
+	optionObjects = [];
 }
 
-function reflect()
+function colorLayers(highlight)
 {
-	// Basically a pastebin, need for later
-	var newX;
-	var newY;
-	if (m != null)
+	for (var layer=0; layer<paperObjects.length; ++layer)
 	{
-		if (paper[i][1] < m*paper[i][0]+b)
+		var color;
+		if (highlight)
 		{
-			// Algorithm for reflecting point across line
-			var d = (paper[i][0] + (paper[i][1]-b)*m)/(1 + m*m);
-			newX = 2*d - paper[i][0];
-			newY = 2*d*m - paper[i][1] + 2*b;
-			paper[i] = [newX, newY];
+			color = "#f698ec";
+		}
+		else
+		{
+			var per = 15;
+			var grey = 255-layer*per;
+			color = {r: grey, g: grey, b: grey};
+		}
+		paperObjects[layer].fill(new SVG.Color(color));
+		paperObjects[layer].remove();
+		paperGroup.add(paperObjects[layer]);
+	}
+}
+
+function intersect(line1, line2)
+{
+	var m = slope(line2); // Slope
+	var b = yIntercept(m, line2[0]); // Equation for y-intercept
+	var n = slope(line1);
+	var x;
+	var y;
+	if (n != null)
+	{
+		var c = yIntercept(n, line1[0]);
+		if (m != null)
+		{
+			x = (c-b)/(m-n); // Equation for intersection of a line
+			y = m*x+b;
+		}
+		else
+		{
+			x = line2[0][0];
+			y = n*x+c;
 		}
 	}
+	else if (m != null)
+	{
+		x = line1[0][0]; // Vertical line: x is any point on it
+		y = m*x+b;
+	}
+	return [x, y];
+}
+
+function yIntercept(m, point)
+{
+	return point[1] - m*point[0];
 }
 
 function pressed(e)
 {
-
-}
-
-function redraw()
-{
-
-
-
 
 }
 
@@ -143,11 +332,6 @@ function slope(line)
         return null;
     else
         return (line[1][1] - line[0][1]) / dx;
-}
-
-function collideLine()
-{
-
 }
 
 function gameX(windowX)
